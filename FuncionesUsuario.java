72f635e2d6efba6ca743b234dece1699059db9b4
Martin Sosa <martin.sito@hotmail.com>
2020-03-30 19:32:14 -0300

    Se agrego la primer funcion

---
+++ FuncionesUsuario.java
@@ -0,0 +1,21 @@
+
+public class FuncionesUsuario {
+    public int calculoDeImpuestos(Usuario user) {
+        float valorAPagar = 0;
+        if (user.getEdad() < 18) {
+            valorAPagar = 0;
+        } else if (user.getEdad() >= 18 && user.getEdad() <= 30) {
+            valorAPagar = 2000;
+        } else if (user.getEdad() >= 30 && user.getEdad() <= 65) {
+            valorAPagar = 7000;
+        } else if (user.getEdad() > 65 ) {
+            valorAPagar = 2000;
+        }
+        float valorAPagarFinal =   valorAPagar - ((valorAPagar * (3 * user.getCantidadDeHijos())) /100) ;
+        if (valorAPagarFinal  < (valorAPagar/2)) {
+            valorAPagarFinal = valorAPagar/2;
+        }
+        // Se restará de ese pago la cantidad de hijos que posee el usuario x 3% (Nunca puede superar un 50% del total)
+        return valorAPagarFinal;
+    }
+}
